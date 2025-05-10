d1d6db1c90f9634a17282e08201cd944755da6ce
yash15-git <yashaswigarg15@gmail.com>
2025-05-11 03:13:40 +0530

    change in server file

--- server.js
+++ server.js
@@ -45,12 +45,11 @@ app.use((err, req, res, next) => {
 });
 
 // For local development
-if (process.env.NODE_ENV !== 'production') {
-    const PORT = process.env.PORT || 8000;
-    app.listen(PORT, '0.0.0.0', () => {
-        console.log(`Server is running on port ${PORT}`);
-    });
-}
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
 
 // Export the Express API
 module.exports = app; 
