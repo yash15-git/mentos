37ccbb1997f0fe05bc501325c0383c95941f133f
yash15-git <yashaswigarg15@gmail.com>
2025-05-11 03:03:32 +0530

    changes in server handling

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.use((err, req, res, next) => {
 // For local development
 if (process.env.NODE_ENV !== 'production') {
     const PORT = process.env.PORT || 8000;
-    app.listen(PORT, () => {
+    app.listen(PORT, '0.0.0.0', () => {
         console.log(`Server is running on port ${PORT}`);
     });
 }
